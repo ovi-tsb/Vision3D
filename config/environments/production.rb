b5016bb0c48c1807279eb47d985da750ca7af24a
ovi-tsb <ovi_tsb@yahoo.com>
2024-04-10 13:00:38 -0400

    Changes to prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,10 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+###########################
+  config.action_mailer.default_url_options = {host: "https://vision3d.onrender.com"}
+  Rails.application.routes.default_url_options[:host] = "https://vision3d.onrender.com"
+
+  config.require_master_key = true
  
 end
